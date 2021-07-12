2305b17619a6a12846571cf7bb37fa24d42aa800
Md Obydul Islam <mdobydulislam100@gmail.com>
2021-07-12 10:37:15 +0600

    lambda expression

--- advance_java_baz_01/src/com/obydul/lambda/ContactService.java
+++ advance_java_baz_01/src/com/obydul/lambda/ContactService.java
@@ -11,8 +11,27 @@ public class ContactService {
 				contactAgedBetween18And25.add(contact);
 			}
 		}
-
 		return contactAgedBetween18And25;
+	}
+
+	public List<Contact> findMaleContactAgedBetween12And25(List<Contact> contacts) {
+		List<Contact> maleAgedBetween18And25 = new ArrayList<Contact>();
+		for (Contact contact : contacts) {
+			if (18 <= contact.getAge() && contact.getAge() <= 25 && contact.getGender() == Contact.Gender.Male) {
+				maleAgedBetween18And25.add(contact);
+			}
+		}
+		return maleAgedBetween18And25;
+	}
+
+	public List<Contact> findFemaleContactAgedBetween12And25(List<Contact> contacts) {
+		List<Contact> femaleAgedBetween18And25 = new ArrayList<Contact>();
+		for (Contact contact : contacts) {
+			if (18 <= contact.getAge() && contact.getAge() <= 25 && contact.getGender() == Contact.Gender.Female) {
+				femaleAgedBetween18And25.add(contact);
+			}
+		}
+		return femaleAgedBetween18And25;
 	}
 
 }

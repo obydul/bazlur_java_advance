739a48e5ef08c6ffc5c8053b19635d67ed5b1257
Md. Obydul Islam <mdobydulislam100@gmail.com>
2021-07-17 07:58:45 +0600

    default method in interface

---
+++ bazlur_java_advance_04/src/com/obydul/defaults/Loggable.java
@@ -0,0 +1,35 @@
+package com.obydul.defaults;
+
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
+public interface Loggable {
+	Logger getLogger();
+
+	String getDelimiter();
+
+	void log(Level level, String message);
+
+	default void info(String message, Object... objects) {
+		getLogger().log(new LogRecord(Level.INFO, formatMessage(message, objects)));
+	}
+
+	default void error(String message, Object... objects) {
+		getLogger().log(new LogRecord(Level.SEVERE, formatMessage(message, objects)));
+	}
+
+	private String formatMessage(String message, Object... args) {
+		StringBuilder sbMessage = new StringBuilder(message);
+
+		for (Object arg : args) {
+			int index = sbMessage.indexOf(getDelimiter());
+			if (index == -1) {
+				break;
+			}
+			sbMessage.replace(index, index + getDelimiter().length(), arg == null ? "null" : arg.toString());
+		}
+		return sbMessage.toString();
+	}
+
+}

be689448aa0694bf1927ec0bbdb0843333108cea
Md Obydul Islam <mdobydulislam100@gmail.com>
2021-07-31 21:14:57 +0600

    test

--- bazlur_java_advance_07/src/com/obydul/strategy/CommunicationApplication.java
+++ bazlur_java_advance_07/src/com/obydul/strategy/CommunicationApplication.java
@@ -3,17 +3,20 @@ package com.obydul.strategy;
 public class CommunicationApplication {
 	public static void main(String[] args) {
 		var communicationService = new CommunicationService();
-		
-		var email = new Email();
+		communicationService.setCommunicable((destination, message) -> "Communicating via Email -> sending"
+				+ "message to destination: [" + destination + "], message:[" + message + "]");
+
+		communicationService.sendMessage("obydul@gmail.com", "hey obydul");
+
+		/*var email = new Email();
 		var phoneCall = new PhoneCall();
 		var videoCall = new VideoCall();
 
 		communicationService.setCommunicable(email);
-		communicationService.sendMessage("obydul@gmail.com", "hey obydul");
 
 		communicationService.setCommunicable(phoneCall);
 		communicationService.sendMessage("+8801523XXXXXX", "hello obydul");
 		communicationService.setCommunicable(videoCall);
-		communicationService.sendMessage("+8801523XXXXXX", "video call");
+		communicationService.sendMessage("+8801523XXXXXX", "video call");*/
 	}
 }

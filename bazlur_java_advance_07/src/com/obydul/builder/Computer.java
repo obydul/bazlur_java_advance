96886d74ecacfb4a1702c78ad7fa4ffa92932407
Md Obydul Islam <mdobydulislam100@gmail.com>
2021-08-02 09:42:17 +0600

    Builder Pattern

---
+++ bazlur_java_advance_07/src/com/obydul/builder/Computer.java
@@ -0,0 +1,33 @@
+package com.obydul.builder;
+
+import java.util.function.Consumer;
+import static com.obydul.builder.Disk.disk;
+import static com.obydul.builder.Processor.processor;
+
+public class Computer {
+	private final Disk disk;
+	private final Processor processor;
+
+	public Computer(Disk disk, Processor processor) {
+		this.disk = disk;
+		this.processor = processor;
+	}
+
+	public static void create(Consumer<ComputerBuilder> builder) {
+		builder.accept(processor -> disk -> new Computer(disk, processor));
+	}
+
+	interface ComputerBuilder {
+		ProcessorBuilder with(Processor processor);
+	}
+
+	interface ProcessorBuilder {
+		Computer and(Disk disk);
+	}
+
+	public static void main(String[] args) {
+		Computer.create(
+				builder -> builder.with(processor().core(2).speed(3).i386()).and(disk().size(512).speed(7200).sata()));
+	}
+
+}

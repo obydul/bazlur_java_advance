a54992ada41ede5efd0530d46f51e60cce619c4d
Md. Obydul Islam <mdobydulislam100@gmail.com>
2021-08-04 18:09:02 +0600

    Generics

--- bazlur_java_advance_08/src/com/obydul/generic/GenericsExample.java
+++ bazlur_java_advance_08/src/com/obydul/generic/GenericsExample.java
@@ -1,9 +1,16 @@
 package com.obydul.generic;
 
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
 public class GenericsExample {
 
 	public static void main(String[] args) {
-
+		List<?> list = Arrays.asList("This", "is", "java", LocalDate.now());
+		for (String string : list) {
+			System.out.println(string);
+		}
 	}
 
 }
